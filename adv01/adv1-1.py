a465b503ea80db31256c573d5ee4b50cb0fdec46
mihirbpi <mihir.borkar@gmail.com>
2021-12-01 09:34:10 -0800

    Finished day 1 part 1

---
+++ adv01/adv1-1.py
@@ -0,0 +1,14 @@
+from aocd import get_data
+
+list = get_data(day=1).split("\n")
+
+previous_measurement = list[0]
+count = 0
+
+for i in range(1, len(list)):
+
+    if(int(list[i]) > int(previous_measurement)):
+        count += 1
+    previous_measurement = list[i]
+
+print(count)
