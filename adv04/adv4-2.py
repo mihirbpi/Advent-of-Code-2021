8e458b26f06609d7cfff366ef7be7f51a9f59ac6
mihirbpi <mihir.borkar@gmail.com>
2021-12-04 12:55:15 -0800

    formatting

--- adv04/adv4-2.py
+++ adv04/adv4-2.py
@@ -7,17 +7,23 @@ boards = input[1:]
 
 def mark(num, boards_list, index):
         board = boards_list[index]
+
         for j in range(0, len(board)):
+
             for k in range(0, len(board[0])):
+                
                 if(board[j][k][0] == num):
                     board[j][k][1] = True
 
 def get_score(num, boards_list, index):
     score = 0
     board = boards_list[index]
+
     for j in range(0, len(board)):
         row = board[j]
+
         for x in row:
+
             if x[1] == False:
                 score += int(x[0])
 
@@ -31,18 +37,24 @@ def is_win(boards_list, index):
     for j in range(0, len(board)):
         bingo = True
         row = board[j]
+
         for x in row:
+
             if x[1] == False:
                 bingo = False
+
         if(bingo):
             return i
 
     for j  in range(0, len(board[0])):
         bingo = True
         column = [x[j] for x in board]
+
         for x in column:
+
             if x[1] == False:
                 bingo = False
+
         if(bingo):
             return i
 
@@ -52,8 +64,10 @@ for board in boards:
     boards_list.append(board.split("\n"))
 
 for board in boards_list:
+
     for i in range (0, len(board)):
         split = board[i].split(" ")
+
         split = [x for x in split if x != ""]
         for j in range(0, len(split)):
             split[j] = [split[j], False]
