dd46aafeb3cea69eb302c905380f8e0283a0179b
mihirbpi <mihir.borkar@gmail.com>
2021-12-05 13:46:07 -0800

    Improve

--- adv05/adv5-1.py
+++ adv05/adv5-1.py
@@ -2,38 +2,25 @@ from aocd import get_data
 from collections import defaultdict
 
 input = get_data(year=2021,day=5).split("\n")
-grid_dict  = defaultdict(lambda: 0)
+vent_map_dict  = defaultdict(lambda: 0)
 
 for i in range(0, len(input)):
     line = input[i]
     p1_string = line.split("->")[0].strip(" ")
     p2_string = line.split("->")[1].strip(" ")
-    p1_x = int(p1_string.split(",")[0])
-    p1_y = int(p1_string.split(",")[1])
-    p2_x = int(p2_string.split(",")[0])
-    p2_y = int(p2_string.split(",")[1])
-    min_x = min(p1_x, p2_x)
-    max_x = max(p1_x, p2_x)
-    min_y = min(p1_y, p2_y)
-    max_y = max(p1_y, p2_y)
-    p1 = [p1_x, p1_y]
-    p2 = [p2_x, p2_y]
+    p1_x, p1_y = map(int, p1_string.split(","))
+    p2_x, p2_y = map(int, p2_string.split(","))
 
     if(p1_y == p2_y):
 
-        for x in range(min_x, max_x + 1):
-            grid_dict[(x, p1_y)] += 1
+        for x in range(min(p1_x, p2_x), max(p1_x, p2_x) + 1):
+            vent_map_dict[(x, p1_y)] += 1
 
     elif(p1_x == p2_x):
 
-        for y in range(min_y, max_y + 1):
-            grid_dict[(p1_x, y)] += 1
+        for y in range(min(p1_y, p2_y), max(p1_y, p2_y) + 1):
+            vent_map_dict[(p1_x, y)] += 1
 
-count2_or_more_overlaps = 0
-
-for item in grid_dict.items():
-
-    if(item[1] >= 2):
-        count2_or_more_overlaps += 1
-
+vent_map_dict_list = list(vent_map_dict.items())
+count2_or_more_overlaps = sum(map(lambda x: x[1] >= 2, vent_map_dict_list))
 print(count2_or_more_overlaps)
--- adv05/adv5-2.py
+++ adv05/adv5-2.py
@@ -1,59 +1,37 @@
 from aocd import get_data
 from collections import defaultdict
+import numpy as np
 
 input = get_data(year=2021,day=5).split("\n")
-grid_dict  = defaultdict(lambda: 0)
-
-def bool_to_int(bool):
-
-    if(bool):
-        return -1
-    return 1
+vent_map_dict  = defaultdict(lambda: 0)
 
 for i in range(0, len(input)):
     line = input[i]
     p1_string = line.split("->")[0].strip(" ")
     p2_string = line.split("->")[1].strip(" ")
-    p1_x = int(p1_string.split(",")[0])
-    p1_y = int(p1_string.split(",")[1])
-    p2_x = int(p2_string.split(",")[0])
-    p2_y = int(p2_string.split(",")[1])
-    y_decreasing = p1_y > p2_y
-    x_decreasing = p1_x > p2_x
-    min_x = min(p1_x, p2_x)
-    max_x = max(p1_x, p2_x)
-    min_y = min(p1_y, p2_y)
-    max_y = max(p1_y, p2_y)
-    p1 = [p1_x, p1_y]
-    p2 = [p2_x, p2_y]
+    p1_x, p1_y = map(int, p1_string.split(","))
+    p2_x, p2_y = map(int, p2_string.split(","))
 
     if(p1_y == p2_y):
 
-        for x in range(min_x, max_x + 1):
-            grid_dict[(x, p1_y)] += 1
+        for x in range(min(p1_x, p2_x), max(p1_x, p2_x) + 1):
+            vent_map_dict[(x, p1_y)] += 1
 
     elif(p1_x == p2_x):
 
-        for y in range(min_y, max_y + 1):
-            grid_dict[(p1_x, y)] += 1
+        for y in range(min(p1_y, p2_y), max(p1_y, p2_y) + 1):
+            vent_map_dict[(p1_x, y)] += 1
 
     else:
-        x_decr_int = bool_to_int(x_decreasing)
-        y_decr_int = bool_to_int(y_decreasing)
         x = p1_x
         y = p1_y
-        grid_dict[(x, y)] += 1
+        vent_map_dict[(x, y)] += 1
 
         while(x != p2_x and y != p2_y):
-            x += x_decr_int
-            y += y_decr_int
-            grid_dict[(x, y)] += 1
+            x += np.sign(p2_x - p1_x)
+            y += np.sign(p2_y - p1_y)
+            vent_map_dict[(x, y)] += 1
 
-count2_or_more_overlaps = 0
-
-for item in grid_dict.items():
-
-    if(item[1] >= 2):
-        count2_or_more_overlaps += 1
-
+vent_map_dict_list = list(vent_map_dict.items())
+count2_or_more_overlaps = sum(map(lambda x: x[1] >= 2, vent_map_dict_list))
 print(count2_or_more_overlaps)
