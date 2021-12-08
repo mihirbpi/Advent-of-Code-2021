6286992f2d1b17a6f744796d91e11cd70c83d5a6
mihirbpi <mihir.borkar@gmail.com>
2021-12-08 12:54:47 -0800

    Improve day 8

--- adv08/adv8-1.py
+++ adv08/adv8-1.py
@@ -1,5 +1,4 @@
 from aocd import get_data
 
 input = get_data(year=2021,day=8).split("\n")
-count =  sum([len([digit for digit in line.strip("\n").split(" | ")[1].split(" ") if len(digit) in [2,4,3,7]]) for line in input])
-print(count)
+print(sum([len([digit for digit in line.split(" | ")[1].split(" ") if len(digit) in [2,4,3,7]]) for line in input]))
--- adv08/adv8-2.py
+++ adv08/adv8-2.py
@@ -3,11 +3,6 @@ from collections import defaultdict
 
 input = get_data(year=2021,day=8).split("\n")
 
-def my_sort(l):
-    L = l
-    L.sort()
-    return L
-
 def deduce(signal_to_digit, digit_to_signal, signals):
 
     for signal in [x for x in signals if len(x) == 6]:
@@ -17,58 +12,44 @@ def deduce(signal_to_digit, digit_to_signal, signals):
             if(letter not in signal):
 
                 if(letter in digit_to_signal["4"] and letter not in digit_to_signal["1"]):
-                    signal_to_digit[tuple(my_sort(list(signal)))] = "0"
-                    digit_to_signal["0"] = tuple(my_sort(list(signal)))
+                    signal_to_digit[tuple(sorted(list(signal)))] = "0"
+                    digit_to_signal["0"] = tuple(sorted(list(signal)))
 
                 elif(letter in digit_to_signal["4"] and letter in digit_to_signal["1"]):
-                    signal_to_digit[tuple(my_sort(list(signal)))] = "6"
-                    digit_to_signal["6"] = tuple(my_sort(list(signal)))
+                    signal_to_digit[tuple(sorted(list(signal)))] = "6"
+                    digit_to_signal["6"] = tuple(sorted(list(signal)))
 
                 elif(letter not in digit_to_signal["4"]):
-                    signal_to_digit[tuple(my_sort(list(signal)))] = "9"
-                    digit_to_signal["9"] = tuple(my_sort(list(signal)))
+                    signal_to_digit[tuple(sorted(list(signal)))] = "9"
+                    digit_to_signal["9"] = tuple(sorted(list(signal)))
 
     for signal in [x for x in signals if len(x) == 5]:
 
         if(len(set(digit_to_signal["6"]) - set(signal)) == 1):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "5"
-            digit_to_signal["5"] = tuple(my_sort(list(signal)))
+            signal_to_digit[tuple(sorted(list(signal)))] = "5"
+            digit_to_signal["5"] = tuple(sorted(list(signal)))
 
         elif(len(set(digit_to_signal["9"]) - set(signal)) == 1):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "3"
-            digit_to_signal["3"] = tuple(my_sort(list(signal)))
+            signal_to_digit[tuple(sorted(list(signal)))] = "3"
+            digit_to_signal["3"] = tuple(sorted(list(signal)))
 
         else:
-            signal_to_digit[tuple(my_sort(list(signal)))] = "2"
-            digit_to_signal["2"] = tuple(my_sort(list(signal)))
+            signal_to_digit[tuple(sorted(list(signal)))] = "2"
+            digit_to_signal["2"] = tuple(sorted(list(signal)))
 
 total = 0
+len_dict = defaultdict(lambda: "0")
+len_dict.update({2: "1", 3: "7", 4: "4", 7: "8"})
 
 for line in input:
-    signal_to_digit = defaultdict(lambda: "-1")
-    digit_to_signal = defaultdict(lambda: "-1")
-    signals = line.strip("\n").split(" | ")[0].split(" ")
-    digits = line.strip("\n").split(" | ")[1].split(" ")
+    signal_to_digit, digit_to_signal = 2*[defaultdict(lambda: "-1")]
+    signals, digits = [x.split(" ") for x in line.split(" | ")]
 
     for signal in signals:
-
-        if(len(signal) == 2):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "1"
-            digit_to_signal["1"] = tuple(my_sort(list(signal)))
-
-        elif(len(signal) == 4):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "4"
-            digit_to_signal["4"] = tuple(my_sort(list(signal)))
-
-        elif(len(signal) == 3):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "7"
-            digit_to_signal["7"] = tuple(my_sort(list(signal)))
-
-        elif(len(signal) == 7):
-            signal_to_digit[tuple(my_sort(list(signal)))] = "8"
-            digit_to_signal["8"] = tuple(my_sort(list(signal)))
+        signal_to_digit[tuple(sorted(list(signal)))] = len_dict[len(signal)]
+        digit_to_signal[len_dict[len(signal)]] = tuple(sorted(list(signal)))
 
     deduce(signal_to_digit, digit_to_signal, signals)
-    total += int("".join([signal_to_digit[tuple(my_sort(list(digit)))] for digit in digits]))
+    total += int("".join([signal_to_digit[tuple(sorted(list(digit)))] for digit in digits]))
 
 print(total)
