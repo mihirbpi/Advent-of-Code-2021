672db7beb2e1bb86f20b0b1b2e70dd9f07388633
mihirbpi <mihir.borkar@gmail.com>
2021-12-09 19:49:22 -0800

    Finished day 9 part 1

---
+++ adv09/adv9-1.py
@@ -0,0 +1,16 @@
+from aocd import get_data
+from collections import defaultdict
+
+input = get_data(year=2021,day=9).split("\n")
+height = defaultdict(lambda: 10)
+
+for i in range(0, len(input)):
+
+    for j in range(0, len(input[i])):
+        height[(i,j)] = int(input[i][j].strip())
+
+def is_low_pt(pt, height):
+    i,j = list(pt)
+    return min([height[(i,j+1)], height[(i+1,j)], height[(i-1,j)], height[(i,j-1)]]) > height[(i,j)]
+
+print(sum([1 + height[pt] for pt in list(height.keys()) if is_low_pt(pt, height)]))
