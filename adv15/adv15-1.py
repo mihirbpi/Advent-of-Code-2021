be487654b0349d6669f06070bb6cde2ba61161bc
mihirbpi <mihir.borkar@gmail.com>
2021-12-15 14:24:50 -0800

    Finished day 15

--- adv15/adv15-1.py
+++ adv15/adv15-1.py
@@ -6,34 +6,17 @@ input = get_data(year=2021,day=15).split("\n")
 #Dijkstra's algorithm
 dist = {}
 prev = {}
-length = {}
 Q = []
 
 for y in range(0, len(input)):
 
     for x in range(0, len(input[0])):
         v = (x,y)
-        dist[v] = 1000
+        dist[v] = 1000000
         Q.append(v)
-        n1 = (x+1, y)
-        n2 = (x, y+1)
-        n3 = (x-1, y)
-        n4 = (x, y-1)
-
-        if(x+1 < len(input[y])):
-            length[(v,n1)] = int(input[y][x+1])
-
-        if(y+1 < len(input)):
-            length[(v,n2)] = int(input[y+1][x])
-
-        if(x-1 >= 0):
-            length[(v,n3)] = int(input[y][x-1])
-
-        if(y-1 >= 0):
-            length[(v,n4)] = int(input[y-1][x])
-
 
 source = (0,0)
+target = (len(input[0]) - 1, len(input) - 1)
 dist[source] = 0
 
 while (len(Q) > 0):
@@ -46,8 +29,12 @@ while (len(Q) > 0):
         if (y[1] < min_val):
             min_val = y[1]
             min_element = y[0]
-            
+
     u = min_element
+
+    if(u == target):
+        break
+
     Q.remove(u)
     u_x = u[0]
     u_y = u[1]
@@ -72,14 +59,14 @@ while (len(Q) > 0):
     for v in neighbors:
 
         if(v in Q):
-            alt = dist[u] + length[(u,v)]
+            alt = dist[u] + int(input[v[1]][v[0]])
 
             if (alt < dist[v]):
                 dist[v] = alt
                 prev[v] = u
 
 S = []
-u = (len(input[0]) - 1, len(input) - 1)
+u = target
 
 if (u in prev or u == source):
 
@@ -94,6 +81,6 @@ path_length = 0
 for i in range(0, len(path) - 1):
     curr = path[i]
     next = path[i+1]
-    path_length += length[(curr, next)]
+    path_length += int(input[next[1]][next[0]])
 
 print(path_length)
--- adv15/adv15-2.py
+++ adv15/adv15-2.py
@@ -1,63 +1,42 @@
 from aocd import get_data
 from collections import defaultdict
+import heapdict
 
 input = get_data(year=2021,day=15).split("\n")
 
 #Dijkstra's algorithm
 def get_risk(x, y, input):
     a = int(input[y % len(input)][x % len(input[0])]) + (x// len(input[0])) + (y // len(input))
-    return (a-1) % 9 + 1
+    return (a - 1) % 9 + 1
 
 dist = {}
 prev = {}
-length = {}
-Q = []
+source = (0,0)
+dist[source] = 0
+target = (5 * len(input[0]) - 1, 5 * len(input) - 1)
+Q = heapdict.heapdict()
 
 for y in range(0, 5*len(input)):
 
     for x in range(0, 5*len(input[0])):
         v = (x,y)
-        dist[v] = 1000
-        Q.append(v)
-        n1 = (x+1, y)
-        n2 = (x, y+1)
-        n3 = (x-1, y)
-        n4 = (x, y-1)
-
-        if(x+1 < 5*len(input[0])):
-            length[(v,n1)] = get_risk(x+1, y, input)
-
-        if(y+1 < 5*len(input)):
-            length[(v,n2)] = get_risk(x, y+1, input)
-
-        if(x-1 >= 0):
-            length[(v,n3)] = get_risk(x-1, y, input)
-
-        if(y-1 >= 0):
-            length[(v,n4)] = get_risk(x, y-1, input)
-
-
-source = (0,0)
-dist[source] = 0
+        if(v != source):
+            dist[v] = 1000000
+        Q[v] = dist[v]
 
 while (len(Q) > 0):
-    L = [(x, dist[x]) for x in Q]
-    min_val = 1000
-    min_element = None
+    popped = Q.popitem()
+    u = popped[0]
 
-    for y in L:
+    if(u == target):
+        break
 
-        if (y[1] < min_val):
-            min_val = y[1]
-            min_element = y[0]
-    u = min_element
-    Q.remove(u)
     u_x = u[0]
     u_y = u[1]
     neighbors = []
 
-    n1 = (u_x+1, u_y)
-    n2 = (u_x, u_y+1)
+    n1 = (u_x + 1, u_y)
+    n2 = (u_x, u_y + 1)
     n3 = (u_x - 1, u_y)
     n4 = (u_x, u_y - 1)
 
@@ -70,34 +49,35 @@ while (len(Q) > 0):
     if(u_x - 1 >= 0):
         neighbors.append(n3)
 
-    if(u_y - 1>= 0):
+    if(u_y - 1 >= 0):
         neighbors.append(n4)
 
     for v in neighbors:
 
         if(v in Q):
-            alt = dist[u] + length[(u,v)]
+            alt = dist[u] + get_risk(v[0], v[1], input)
 
             if (alt < dist[v]):
                 dist[v] = alt
                 prev[v] = u
+                Q[v] = alt
 
 S = []
-u = (5*len(input[0]) - 1, 5*len(input) - 1)
+u = target
+
 if (u in prev or u == source):
+
     while (u in prev):
         S.append(u)
         u = prev[u]
 
 S.append(source)
 path = list(reversed(S))
-
-
 path_length = 0
 
 for i in range(0, len(path) - 1):
     curr = path[i]
     next = path[i+1]
-    path_length += length[(curr, next)]
+    path_length += get_risk(next[0], next[1], input)
 
 print(path_length)
