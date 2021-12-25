e91c3117d200c3d0232e597006ca50996708996b
mihirbpi <mihir.borkar@gmail.com>
2021-12-25 11:50:50 -0800

    formatting

--- adv25/adv25-1.py
+++ adv25/adv25-1.py
@@ -17,16 +17,13 @@ def do_step(cucumber_dict):
     east_moves = 0
     south_moves = 0
 
-
     for pos in cucumber_dict:
         after_east_cucumber_dict[pos] = cucumber_dict[pos]
 
         if (cucumber_dict[pos] == ">"):
             east_facing_poses.append(pos)
 
-
     for pos in east_facing_poses:
-
         adjacent_pos = (pos[0], (pos[1] + 1) % len(input[0]))
 
         if(cucumber_dict[adjacent_pos] == "."):
@@ -34,17 +31,13 @@ def do_step(cucumber_dict):
             after_east_cucumber_dict[pos] = "."
             after_east_cucumber_dict[adjacent_pos] = ">"
 
-
     for pos in after_east_cucumber_dict:
         after_south_cucumber_dict[pos] = after_east_cucumber_dict[pos]
 
         if (after_east_cucumber_dict[pos] == "v"):
             south_facing_poses.append(pos)
 
-
     for pos in south_facing_poses:
-
-
             adjacent_pos = ((pos[0] + 1) % len(input), pos[1])
 
             if(after_east_cucumber_dict[adjacent_pos] == "."):
